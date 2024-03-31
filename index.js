4d91b55b2e62ec8fb204f7689b820a9ccf1d5d76
SHAIK SHARUKHBABU <130997833+SHARUKH-BABU@users.noreply.github.com>
2024-03-31 10:09:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
+// let animationBlock = document.getElementById("animation-block");
+//     setTimeout(() => {
+//         animationBlock.classList.add("hidden");
+//         let container = document.getElementById('container');
+//         container.classList.remove("hidden");
+// }, 1000);
 
 const invoke = async () => {
-   try{
-        let key =  "0dea7395a6044585b3cc6d989a0d48a0";
-        let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=${key}`;
-        let data  = await fetch(url);
-        let parsedData = await data.json();
-    
-        // Access the articles array from the parsedData
-        let articles = parsedData.articles;
-        console.log(articles)
-    
-        // Loop through the articles array and perform DOM manipulation
-        articles.forEach((article, index) => {
-            let container = document.getElementById('container');
-            let newsItem = document.createElement('div');
-            newsItem.classList.add('news-item', 'm-5', 'rounded-lg', 'bg-violet-500', 'p-5', 'shadow-2xl', 'shadow-violet-950');
-            newsItem.innerHTML = `
-                <img class="w-full max-h-[50%]" src="${article.urlToImage}" alt="${article.title}" />
-                <h1 class="my-2 text-center font-bold text-white">${article.title}</h1>
-                <p class="text-slate-950">${article.description ? article.description.slice(0, 100)+"....." : ""}</p>
-                <button class="mt-3 rounded bg-red-500 p-2 text-white ring-2 hover:bg-red-600"><a href="${article.url}" target="_blank">READ MORE ...</a></button>
-            `;
-            container.appendChild(newsItem);
-        });
-   }
-    catch(error){
-        console.log("error occured : "+error.message)
-    }
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=0dea7395a6044585b3cc6d989a0d48a0`;
+    let data = await fetch(url)
+    let parsedData = await data.json();
+
+    // Access the articles array from the parsedData
+    let articles = parsedData.articles;
+    console.log(articles)
+
+    // Loop through the articles array and perform DOM manipulation
+    articles.forEach((article, index) => {
+        let container = document.getElementById('container');
+        let newsItem = document.createElement('div');
+        newsItem.classList.add('news-item', 'm-5', 'rounded-lg', 'bg-violet-500', 'p-5', 'shadow-2xl', 'shadow-violet-950');
+        newsItem.innerHTML = `
+            <img class="w-full max-h-[50%]" src="${article.urlToImage}" alt="${article.title}" />
+            <h1 class="my-2 text-center font-bold text-white">${article.title}</h1>
+            <p class="text-slate-950">${article.description ? article.description.slice(0, 100)+"....." : ""}</p>
+            <button class="mt-3 rounded bg-red-500 p-2 text-white ring-2 hover:bg-red-600"><a href="${article.url}" target="_blank">READ MORE ...</a></button>
+        `;
+        container.appendChild(newsItem);
+    });
 }
 
-invoke();
-
+invoke()
